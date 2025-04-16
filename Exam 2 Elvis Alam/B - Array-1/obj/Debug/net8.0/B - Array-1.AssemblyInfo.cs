5c60e47b86b0ce90c27acf046f9a36f25aeb7028
Elvis Alam <alamelvism@gmail.com>
2025-04-16 17:52:12 -0500

    Searching and Sorting Algorithms

--- Exam 2 Elvis Alam/B - Array-1/obj/Debug/net8.0/B - Array-1.AssemblyInfo.cs
+++ Exam 2 Elvis Alam/B - Array-1/obj/Debug/net8.0/B - Array-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B - Array-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332621918f6f1758976ca051d3aa1d5c9aa25323")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50834f774beab88ae8a01cefcf01af03af184fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("B - Array-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B - Array-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Session 13/Searching/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Searching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 5, 10, 27, 30, 34, 51, 56, 65, 77, 81, 82, 93, 99 };
+            Console.WriteLine("Enter the item.");
+            int item = Convert.ToInt32(Console.ReadLine());
+            int index = BinarySearch(array, item);
+
+            if (index >= 0)
+            {
+                Console.WriteLine($"The item with a value of {array[index]} is found at position {index}.");
+            }
+            else
+            {
+                Console.WriteLine("The item is not found in the array");
+
+            }
+        }
+
+        static int LinearSearch(int[] a, int item)
+        {
+            int index = -1;
+            // O(n)
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (item == a[i])
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        static int BinarySearch(int[] a, int item)
+        {
+            int index = -1;
+            int low = 0;
+            int high = a.Length - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (item > a[mid])
+                {
+                    low = mid + 1;
+                }
+                if (item < a[mid])
+                {
+                    high = mid - 1;
+                }
+                if (item == a[mid])
+                {
+                    index = mid;
+                    break;
+                }
+
+            }
+
+            return index;
+        }
+    }
+}
